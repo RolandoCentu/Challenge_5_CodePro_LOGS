38eb5f4512b9d616f9fe7d96116938661e77cbb4
Rolando Centurión <rolandocena@gmail.com>
2026-03-20 18:31:11 -0300

    Initialize Flask server with token validation

--- Server.py
+++ Server.py
@@ -1,1 +1,12 @@
 
+rom flask import Flask, request, jsonify      # importamos flask para crear el servidor de logging
+import sqlite3
+from datetime import datetime
+
+app = Flask(__name__)        # creamos una instancia de la clase Flask para nuestro servidor 
+
+# Definimos tokens validos para autenticar los servicios que envían logs, 1 token es invalido
+TOKENS_VALIDOS = {
+    "token_service_a": "service_a",
+    "token_service_b": "service_b",
+    "token_service_c": "service_c"}
